d8d1d9ab19232bc3712e0ea75cb0d5e4e95398a7
the0jhaji <adarshkumarojha.aiml2024@nsec.ac.in>
2026-04-16 20:36:39 +0530

    else with try except

---
+++ ExceptionHandeling/ex4.py
@@ -0,0 +1,12 @@
+try:
+    lst=[10, 20 ,30, 40, 50]
+    for num in lst:
+        i=int(num)
+        j=i*i
+        print(i, j)
+
+except NameError:
+    print(NameError.args)
+else:
+    print('total numbers processed', len(lst))
+    del(lst)
