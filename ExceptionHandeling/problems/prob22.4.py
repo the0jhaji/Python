356c388c920aa82f3cbb6a83b7560ecde6371c7e
the0jhaji <adarshkumarojha.aiml2024@nsec.ac.in>
2026-04-16 21:26:56 +0530

    problem 22.4

---
+++ ExceptionHandeling/problems/prob22.4.py
@@ -0,0 +1,12 @@
+# Write a program that receives an integer as input. If a string is entered
+# instead of an integer, then report an error and give another chance to
+# user to enter an integer. Continue this process till correct input is
+# supplied.
+
+while True:
+    try:
+        num=int(input("Enter a number:"))
+        break
+    except ValueError:
+        print("Incorrect Input")
+print('You entered:', num)
